7a9ceaf08d358999a0fa845d60a215f302d36fe5
nakimera <prossienakimera@gmail.com>
2018-07-31 00:06:06 +0300

    user model

---
+++ app/api/v1/auth/models.py
@@ -0,0 +1,15 @@
+class User(object):
+    """
+    This class defines the app users
+    """
+
+    def __init__(self, username, email_address, password):
+        self.username = username
+        self.email_address = email_address
+        self.password = password
+
+    def user_signup(self):
+        return ''
+
+    def user_login(self):
+        return ''
