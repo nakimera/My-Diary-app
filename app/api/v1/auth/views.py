55474e2fc2a2ba7252617f2d838fad9a18993c12
nakimera <prossienakimera@gmail.com>
2018-07-31 07:46:07 +0300

    login and sign up routes

--- app/api/v1/auth/views.py
+++ app/api/v1/auth/views.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
+from .models import User
 
 mod = Blueprint('auth', __name__)
+
+@mod.route('/signup', methods=['POST'])
+def signup():
+    return make_response('User successfully signed up', 200)
+
+@mod.route('/login', methods=['POST'])
+def login():
+    return make_response('You are successfully logged in', 200)
