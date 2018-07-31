a65df9826de175415d34a922f0933456453e2a02
nakimera <prossienakimera@gmail.com>
2018-07-31 12:35:22 +0300

    add connection class

--- app/api/v1/auth/models.py
+++ app/api/v1/auth/models.py
@@ -1,15 +1,40 @@
-class User(object):
+import psycopg2
+
+class Connection:
+
+    def __init__(self):
+        self.conn = psycopg2.connect(URL)
+
+    def execute_query(self):
+        """
+        This method executes a query inthe database
+        """
+
+        try:
+            cur = self.conn.cursor()
+            cur.execute(query)
+
+        except psycopg2.DatabaseError as ex:
+            self.conn.close()
+
+    def close_connection(self):
+        """
+        This method closes the database connection
+        """
+        self.conn.close()
+
+class User(Connection):
     """
     This class defines the app users
     """
 
     def __init__(self, username, email_address, password):
-        self.user_id = user_id
         self.username = username
         self.email_address = email_address
         self.password = password
 
-    def user_signup(self):
+    def add_user(self):
+
         return ''
 
     @staticmethod
