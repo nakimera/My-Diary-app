30c34b46192f0c4d5319bfba5f9f257eb26a5b77
nakimera <prossienakimera@gmail.com>
2018-07-31 17:25:41 +0300

    create users table

--- app/db.py
+++ app/db.py
@@ -1,16 +1,25 @@
 import psycopg2
 from app.config import DevelopmentConfig
 
-class Connection:
+class DatabaseConnection:
+    """
+    Class to setup a database connection 
+    """
 
     def __init__(self):
-        self.conn = psycopg2.connect(DevelopmentConfig.DATABASE_URL)
+        self.conn = psycopg2.connect(DevelopmentConfig.DATABASE_URL)        
 
-    def execute_query(self, query):
+    def create_users_table(self):
         """
-        This method executes a query in the database
+        Method that creates the users table if it doesn't exist
         """
-
+        query = """CREATE TABLE IF NOT EXISTS users
+                    (user_id SERIAL PRIMARY KEY,
+                    username VARCHAR(30) NOT NULL UNIQUE,
+                    email_address VARCHAR(50) UNIQUE NOT NULL,
+                    password)
+                """
+        
         try:
             cur = self.conn.cursor()
             cur.execute(query)
@@ -19,6 +28,7 @@ class Connection:
         except psycopg2.DatabaseError as ex:
             self.conn.close()
             print(ex)
+
 
     def close_connection(self):
         """
