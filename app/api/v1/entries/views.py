5ade941b4a05a03f78059871b00894e7248c39b9
nakimera <prossienakimera@gmail.com>
2018-07-31 07:48:05 +0300

    routes for create, get and  modify entries

--- app/api/v1/entries/views.py
+++ app/api/v1/entries/views.py
@@ -2,3 +2,23 @@ from flask import Blueprint, make_response
 from .models import Entry
 
 mod = Blueprint('entry', __name__)
+
+@mod.route('/entries', methods=['POST', 'GET'])
+def entries():
+    # method to add an entry
+    if requset.method =='POST':
+        return make_response('Entry succesfully added', 200)
+
+    # method to retrieve all entries
+    if requset.method =='GET':
+        return make_response('Entries succesfully retrieved', 200)
+
+@mod.route('/entries/<entriesId>', methods=['PUT', 'GET'])
+def entry_by_id():
+    # method to get an entry by id
+    if requset.method =='GET':
+        return make_response('Entry succesfully retrieved', 200)
+
+    # method to modify an entry
+    if requset.method =='PUT':
+        return make_response('Entry succesfully modified', 200)
