871fd74c3323975eb9b46e2805d19411bf210579
nakimera <prossienakimera@gmail.com>
2018-07-31 12:32:46 +0300

    add DB url

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
+db_user = 'postgres'
+user_password = 'postgres'
+db_name = 'mydiary'
+
 class Config:
-    SECRET_KEY = "8AZsk5bD2FQl9oXSv4fuxlRxxZnHsV4y"
+    SECRET_KEY = "TcQsWISFjRG4243XobHPIaDxMioisOba"
     DEBUG = False
     TESTING = False 
     DEVELOPMENT = False
@@ -7,14 +11,16 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
+    DATABASE_URL = 'postgresql://'+ db_user +': '+ user_password + '@localhost/'+ db_name +''
 
 class TestingConfig(Config):
     TESTING = True
+    DATABASE_URL = ''
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
     
-configuration = {
+app_config = {
     "development" : DevelopmentConfig,
     "testing" : TestingConfig,
     "production" : ProductionConfig
