9b9e9d3720147d587b9815b69effedf61b030935
nakimera <prossienakimera@gmail.com>
2018-07-31 08:04:52 +0300

    import request

--- app/api/v1/entries/views.py
+++ app/api/v1/entries/views.py
@@ -1,22 +1,22 @@
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request
 from .models import Entry
 
 mod = Blueprint('entry', __name__)
 
-@mod.route('/entries', methods=['POST', 'GET'])
+@mod.route('', methods=['POST', 'GET'])
 def entries():
     # method to add an entry
-    if requset.method =='POST':
+    if request.method =='POST':
         return make_response('Entry succesfully added', 200)
 
     # method to retrieve all entries
-    if requset.method =='GET':
+    if request.method =='GET':
         return make_response('Entries succesfully retrieved', 200)
 
-@mod.route('/entries/<entriesId>', methods=['PUT', 'GET'])
+@mod.route('/<entriesId>', methods=['PUT', 'GET'])
 def entry_by_id():
     # method to get an entry by id
-    if requset.method =='GET':
+    if request.method =='GET':
         return make_response('Entry succesfully retrieved', 200)
 
     # method to modify an entry
