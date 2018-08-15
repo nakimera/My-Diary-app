34bdcb38854317648b2f4c07e2b991f3b9bb1fc8
nakimera <prossienakimera@gmail.com>
2018-08-15 22:10:07 +0300

    edit user tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -6,39 +6,63 @@ class UserTests(BaseClass):
 
     # Test api can create a user
     def test_create_user(self):
-        response = self.client.post('/api/v1/auth/signup', data=self.user)
+        response = self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.user, 
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 201)
         self.assertIn('User successfully signed up', str(response.data))
 
 
     # Test api can not create a user without a username
     def test_cannot_create_user_without_username(self):
-        response = self.client.post('/api/v1/auth/signup', data=self.missing_username)
+        response = self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.missing_username, 
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide a username", str(response.data))
 
 
     # Test api can not create a user without an email address
     def test_cannot_create_user_without_email(self):
-        response = self.client.post('/api/v1/auth/signup', data=self.missing_email)
+        response = self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.missing_email, 
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide an email address", str(response.data))
 
     # Test api can not create a user without an invalid email address
     def test_cannot_create_user_with_an_invalid_email(self):
-        response = self.client.post('/api/v1/auth/signup', data=self.invalid_email)
+        response = self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.invalid_email,
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide a valid email address", str(response.data))
     
     # # Test api can not create a user without a password
     def test_cannot_create_user_without_password(self):
-        response = self.client.post('/api/v1/auth/signup', data=self.missing_password)
+        response = self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.missing_password,
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide a password", str(response.data))
 
     # # Test api can not create a user that already exists
     def test_cannot_create_an_existing_user(self): 
-        self.client.post('/api/v1/auth/signup', data=self.user)
+        self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.user,
+            content_type="application/json"
+        )
         response = self.client.post('/api/v1/auth/signup', data=self.user)
         self.assertEqual(response.status_code, 409)
         self.assertIn("User already exists. Please log in", str(response.data))
@@ -46,22 +70,38 @@ class UserTests(BaseClass):
 
     # # Test user can not login without a username
     def test_user_can_not_log_in_without_username(self):
-        response = self.client.post('/api/v1/auth/login', data=self.missing_username)
+        response = self.client.post(
+            '/api/v1/auth/login', 
+            data=self.missing_username,
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide a username", str(response.data))
 
 
     # # Test user can not login without a password
     def test_user_can_not_log_in_without_password(self):
-        response = self.client.post('/api/v1/auth/login', data=self.missing_password)
+        response = self.client.post(
+            '/api/v1/auth/login', 
+            data=self.missing_password,
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please provide a password", str(response.data))
 
 
     # # Test api can log in a user and generate a token
     def test_user_can_log_in(self):
-        self.client.post('/api/v1/auth/signup', data=self.user)
-        response = self.client.post('/api/v1/auth/login', data=self.user)
+        self.client.post(
+            '/api/v1/auth/signup', 
+            data=self.user,
+            content_type="application/json"
+        )
+        response = self.client.post(
+            '/api/v1/auth/login', 
+            data=self.user,
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
         self.assertIn("You have successfully logged in", str(response.data))
         token = json.loads(response.data.decode())["token"]
