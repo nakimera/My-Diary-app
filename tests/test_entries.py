1aeb9c16d2b55cd269334b535c376cf170dd3b58
nakimera <prossienakimera@gmail.com>
2018-08-15 22:12:58 +0300

    test add entry

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -6,16 +6,13 @@ from tests.test_base import BaseClass
 class EntryTests(BaseClass):
     """This class represents the Entrytest case"""
 
-    # authourization
-    def auth(self):
-            self.client.post('/api/v1/auth/signup', data=self.user)
-            response = self.client.post('/api/v1/auth/login', data=self.user)
-            token = json.loads(response.data.decode())["token"]
-            return token 
-
     # Test api can not add an entry without a token
-    def test_create_entry(self):
-        response = self.client.post('/api/v1/entries', data=self.entry, content_type="application/json")
+    def test_missing_token(self):
+        response = self.client.post(
+            '/api/v1/entries', 
+            data=self.entry, 
+            content_type="application/json"
+        )
         self.assertEqual(response.status_code, 403)
         self.assertIn('Token is missing', str(response.data))
 
@@ -27,5 +24,33 @@ class EntryTests(BaseClass):
             content_type="application/json", 
             headers={ "access-token": token
         })
-        # self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 403)
         self.assertIn('Invalid token. Please try again.', str(response.data))
+
+    # Test api cannot add an entry with missing title
+    def test_entry_title(self):
+        self.client.post('/api/v1/auth/signup', data=self.user)
+        auth = self.client.post('/api/v1/auth/login', data=self.user)
+        token = json.loads(auth.data.decode())["token"]
+        response = self.client.post(
+            '/api/v1/entries', data=self.missing_title, 
+            content_type="application/json", 
+            headers={ "access-token": token
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Please enter a title', str(response.data))
+        
+
+    # Test api can successfully add an entry
+    def test_add_entry(self):
+        self.client.post('/api/v1/auth/signup', data=self.user)
+        rev = self.client.post('/api/v1/auth/login', data=self.user)
+        token = json.loads(rev.data.decode())["token"]
+        response = self.client.post(
+            '/api/v1/entries', data=self.entry, 
+            content_type="application/json", 
+            headers={ "access-token": token
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('Entry successfully added', str(response.data))
+        
